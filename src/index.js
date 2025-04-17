37e8f3d92a1846da1c175e5ce12c15a0c976d889
Dmstry <solarr@ukr.net>
2025-04-17 16:24:03 +0300

    Factorial calculator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('Hello TS again!');
+"use strict";
+// const firstName: string = 'Petro';
+// const lastName: string = 'Ivanov';
+// const pi: number = 3.14;
+// console.log(`${firstName} ${lastName}`);
+// +Вивести 10 консоль.логів у консоль
+// Вивести тільки парні
+// for (let i = 0; i < 10; i++) {
+//   console.log(`Число з циклу for - ${i}`);
+// }
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`Парне число - ${i}`);
+//   }
+// }
+// console.dir(document);
+// Задача: створити невеличкий додаток, який може обчислити факторіал числа
+const form = document.querySelector('#factorial-form');
+const stepsList = document.querySelector('#factorial-steps');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const numberInput = document.querySelector('#number');
+    const number = Number(numberInput.value);
+    let factorial = 1;
+    stepsList.innerHTML = '';
+    for (let i = 1; i <= number; i++) {
+        factorial *= i;
+        const listItem = document.createElement('li');
+        listItem.textContent = `${factorial}`;
+        stepsList.appendChild(listItem);
+    }
+});
