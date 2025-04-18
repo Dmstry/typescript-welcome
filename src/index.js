9897fe68bb274bb2da00e24e8fa18cee0d13dae0
Dmstry <solarr@ukr.net>
2025-04-18 15:53:39 +0300

    Add README.md

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-"use strict";
-// const firstName: string = 'Petro';
-// const lastName: string = 'Ivanov';
-// const pi: number = 3.14;
-// console.log(`${firstName} ${lastName}`);
-// +Вивести 10 консоль.логів у консоль
-// Вивести тільки парні
-// for (let i = 0; i < 10; i++) {
-//   console.log(`Число з циклу for - ${i}`);
-// }
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 === 0) {
-//     console.log(`Парне число - ${i}`);
-//   }
-// }
-// console.dir(document);
-// Задача: створити невеличкий додаток, який може обчислити факторіал числа
-const form = document.querySelector('#factorial-form');
-const stepsList = document.querySelector('#factorial-steps');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const numberInput = document.querySelector('#number');
-    const number = Number(numberInput.value);
-    let factorial = 1;
-    stepsList.innerHTML = '';
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
-        const listItem = document.createElement('li');
-        listItem.textContent = `${factorial}`;
-        stepsList.appendChild(listItem);
-    }
-});
+const hasDriverLicense = false;
+if (hasDriverLicense) {
+    console.log('Користувач може управляти транспортом');
+}
+else {
+    console.log('У користувача намає права управляти транспортом');
+}
