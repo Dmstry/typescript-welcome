6b4aa205e1657e2a3162388b712841c30494e3a4
Dmstry <solarr@ukr.net>
2025-04-22 18:46:34 +0300

    Implement logic find student with highest average grade

--- src/index.js
+++ src/index.js
@@ -1,57 +1,11 @@
-/*
-Задача 1.
-Дано масив чисел.
-Перебрати масив, знайти найбільше число.
-*/
-const numbers = [4, 2, 7, 1, 9, 5];
-let maxNumber = numbers[0];
-for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > maxNumber) {
-        maxNumber = numbers[i];
-    }
-}
-// console.log('Найбільший елемент масиву: ', maxNumber);
-/*
-Задача 2.
-Дано масив чисел.
-Знайти середнє значення у цьому масиві чисел.
-*/
-const numbers2 = [10, 20, 30, 40, 50];
-let sum = 0;
-for (let num of numbers2) {
-    sum += num;
-}
-const avarage = sum / numbers2.length;
-// console.log('Середнє значення числе у масиві: ', avarage);
-/*
-Задача 3.
-Дано масив чисел.
-Знайти перше від'ємне число у масиві.
-*/
-const numbers3 = [10, -20, 30, -40, 50];
-let firstNegative;
-for (let num of numbers3) {
-    if (num < 0) {
-        firstNegative = num;
-        break;
-    }
-}
-const item = { name: 'Футболка', price: 20 };
-const discount = 10; // 10% знижки
-const discountAmount = (discount / 100) * item.price;
-const totalCost = item.price - discountAmount;
-const john = { name: 'John', hourlyRate: 10 };
-const johnAmount = john.hourlyRate * 40; // сумма зарплати, яка виплачується Джону
-const invoice = { name: john.name, amount: johnAmount };
-const books = [
-    { title: 'Грокаємо алгоритми', author: 'Адітья Бхаргва', price: 25 },
-    { title: 'Мова програмування C++', author: 'Бьерн Страуструп', price: 20 },
-    {
-        title: 'Гаррі Поттер і філософський камінь',
-        author: 'Джоан Роулінг',
-        price: 10,
-    },
+const students = [
+    { name: 'Bob', course: 1, averageGrade: 9 },
+    { name: 'Holly', course: 2, averageGrade: 10 },
+    { name: 'John', course: 2, averageGrade: 11 },
+    { name: 'Mike', course: 3, averageGrade: 12 },
 ];
-for (let book of books) {
-    console.log(`Книга: ${book.title}. Автор: ${book.author}. Ціна: ${book.price}$.`);
-}
+const totalAverageGrade = students.reduce((sum, student) => sum + student.averageGrade, 0) /
+    students.length;
+console.log('Загальний середій бал усіх студентів: ', totalAverageGrade);
+const topStudent = students.reduce((maxStudent, student) => student.averageGrade > maxStudent.averageGrade ? student : maxStudent, students[0]);
+console.log(`Студент ${topStudent.course} курсу ${topStudent.name} має найвищий бал - ${topStudent.averageGrade}`);
